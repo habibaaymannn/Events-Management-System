0d79b1a2187ec2eba31a82551b6d64ff5df38faf
habiba <habibaayman100205@gmail.com>
2025-09-03 20:18:49 +0400

    removed notificstionService Interface

--- src/main/java/com/example/cdr/eventsmanagementsystem/Service/Notifications/EmailNotificationService.java
+++ src/main/java/com/example/cdr/eventsmanagementsystem/Service/Notifications/EmailNotificationService.java
@@ -11,7 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class EmailNotificationService implements NotificationService {
+public class EmailNotificationService{
 
     private final AttendeeNotificationService attendeeNotificationService;
     private final PaymentNotificationService paymentNotificationService;
@@ -19,57 +19,47 @@ public class EmailNotificationService implements NotificationService {
     private final OrganizerNotificationService organizerNotificationService;
 
     /// Payment
-    @Override
     public void sendPaymentRequestEmail(Booking booking, String clientSecret) {
         // organizer gets email of payment link
         paymentNotificationService.sendPaymentRequestEmail(booking, clientSecret);
     }
-    @Override
     public void sendPaymentFailureEmail(Booking booking, String failureReason) {
         // organizer gets email of payment failure
        paymentNotificationService.sendPaymentFailureEmail(booking, failureReason);
     }
 
     /// providers
-    @Override
     public void sendProviderBookingEmail(Booking booking) {
         // Notify providers (new booking)
         providerNotificationService.sendNewBookingRequestEmail(booking);
     }
-    @Override
     public void sendProviderConfirmationEmail(Booking booking){
         // Notify providers of booking confirmation
         providerNotificationService.sendBookingConfirmationEmail(booking);
     }
-    @Override
     public void sendProviderCancellationEmail(Booking booking, String reason) {
         // Notify providers of booking cancellation
         providerNotificationService.sendBookingCancellationEmail(booking, reason);
     }
 
     /// organizer
-    @Override
     public void sendBookingConfirmationEmail(Booking booking){
         organizerNotificationService.sendBookingConfirmationEmail(booking);
     }
-    @Override
     public void sendServiceUpdateEmail(Booking booking) {
         // notify organizer the status update(accept/reject)
         organizerNotificationService.sendServiceBookingUpdateEmail(booking);
     }
-    @Override
     public void sendBookingCancellationEmail(Booking booking) {
         // notify organizer when their venue/service booking is cancelled
         organizerNotificationService.sendBookingCancellationEmail(booking);
     }
-    @Override
     public void sendEventCancellationEmail(EventBooking booking) {
         // notify organizer when event is cancelled
         organizerNotificationService.sendEventCancellationEmail(booking);
     }
 
     /// attendee
-    @Override
     public void sendAttendeeCancellationEmail(EventBooking booking) {
         attendeeNotificationService.sendBookingCancellationEmail(booking);
     }
