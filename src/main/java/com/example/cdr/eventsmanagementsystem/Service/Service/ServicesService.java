2e056ea37efdb39706c76a20289d3bba8337ccd3
habiba <habibaayman100205@gmail.com>
2025-09-20 10:27:21 +0400

    Fixes #45: Add getServicesByServiceProvider endpoint
    Added a new endpoint to retrieve services by service provider
    Updated serviceApi to call this endpoint instead of getAllServices
    Ordered endpoints to follow Get, Create, Update, Delete

--- src/main/java/com/example/cdr/eventsmanagementsystem/Service/Service/ServicesService.java
+++ src/main/java/com/example/cdr/eventsmanagementsystem/Service/Service/ServicesService.java
@@ -2,7 +2,6 @@ package com.example.cdr.eventsmanagementsystem.Service.Service;
 
 import com.example.cdr.eventsmanagementsystem.DTO.Service.ServicesDTO;
 import com.example.cdr.eventsmanagementsystem.Mapper.ServiceMapper;
-import com.example.cdr.eventsmanagementsystem.Model.Service.Availability;
 import com.example.cdr.eventsmanagementsystem.Model.User.ServiceProvider;
 import com.example.cdr.eventsmanagementsystem.Repository.ServiceRepository;
 import com.example.cdr.eventsmanagementsystem.Model.Service.Services;
@@ -15,6 +14,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
+
 /**
  * Service class for managing services.
  * Provides functionality to create, updateAvailability
@@ -27,6 +29,17 @@ public class ServicesService {
     private final UserSyncService userSyncService;
     private final ServiceMapper serviceMapper;
 
+    public ServicesDTO getServiceById(Long serviceId) {
+        Services service = serviceRepository.getServiceById(serviceId);
+        return serviceMapper.toServiceDTO(service);
+    }
+
+    public Page<ServicesDTO> getServicesByServiceProvider(Pageable pageable) {
+        ServiceProvider serviceProvider = ensureCurrentUserAsServiceProvider();
+        Page<Services> services = serviceRepository.findByServiceProvider(serviceProvider, pageable);
+        return services.map(serviceMapper::toServiceDTO);
+    }
+
     public Page<ServicesDTO> getAllServices(Pageable pageable) {
         Page<Services> services = serviceRepository.findAll(pageable);
         return services.map(serviceMapper::toServiceDTO);
@@ -35,46 +48,37 @@ public class ServicesService {
     @Transactional
     public ServicesDTO addService(ServicesDTO dto) {
         Services newService = serviceMapper.toService(dto);
-
-        ServiceProvider serviceProvider = userSyncService.ensureUserExists(ServiceProvider.class);
+        ServiceProvider serviceProvider = ensureCurrentUserAsServiceProvider();
         newService.setServiceProvider(serviceProvider);
-
         Services savedService = serviceRepository.save(newService);
         return serviceMapper.toServiceDTO(savedService);
     }
 
     @Transactional
     public ServicesDTO updateService(Long serviceId, ServicesDTO dto) {
-        Services service = serviceRepository.findById(serviceId)
-                .orElseThrow(() -> new EntityNotFoundException("Service not found"));
-
-        String serviceProviderId = AuthUtil.getCurrentUserId();
-        if (!service.getServiceProvider().getKeycloakId().equals(serviceProviderId)) {
-            throw new AccessDeniedException("You are not allowed to update this service");
-        }
-
+        Services service = serviceRepository.getServiceById(serviceId);
+        verifyAccess(serviceId);
         serviceMapper.updateService(dto, service);
-
         Services updatedVenue = serviceRepository.save(service);
         return serviceMapper.toServiceDTO(updatedVenue);
     }
 
-    public ServicesDTO getServiceById(Long serviceId) {
-        Services service = serviceRepository.findById(serviceId).orElseThrow(() -> new EntityNotFoundException("Service not found"));
-        return serviceMapper.toServiceDTO(service);
-    }
-
     @Transactional
     public void deleteService(Long serviceId) {
-        Services service = serviceRepository.findById(serviceId)
-                .orElseThrow(() -> new EntityNotFoundException("Service not found"));
+        verifyAccess(serviceId);
+        serviceRepository.deleteById(serviceId);
+    }
 
+    private void verifyAccess(Long serviceId) throws AccessDeniedException {
+        Services service = serviceRepository.findById(serviceId).orElseThrow(() -> new EntityNotFoundException("Service not found"));
         String serviceProviderId = AuthUtil.getCurrentUserId();
 
-        if (!service.getServiceProvider().getKeycloakId().equals(serviceProviderId)) {
-            throw new AccessDeniedException("You are not allowed to delete this service");
+        if (Objects.isNull(service.getServiceProvider())|| !service.getServiceProvider().getKeycloakId().equals(serviceProviderId)) {
+            throw new AccessDeniedException("You are not allowed to access this service");
         }
+    }
 
-        serviceRepository.deleteById(serviceId);
+    private ServiceProvider ensureCurrentUserAsServiceProvider() {
+        return userSyncService.ensureUserExists(ServiceProvider.class);
     }
 }
